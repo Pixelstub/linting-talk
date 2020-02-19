fa66df617a0480ca8b223cd26bf83efce07184bb
Ashton Honnecke <ashton@digitalassetsdata.com>
2020-02-19 12:55:09 -0700

    revert linting

--- bin/ecsName2IP.py
+++ bin/ecsName2IP.py
@@ -6,15 +6,29 @@ import os
 
 import boto3
 
-DOCKERARN = "arn:aws:iam::1234567890:role/dev-ashton-honnecke-role"
-WEBARN = "arn:aws:iam::1234567891:role/dev-ashton-honnecke-role"
+DOCKERARN = 'arn:aws:iam::712182686879:role/dev-ashton-honnecke-role'
+WEBARN = 'arn:aws:iam::974666675768:role/dev-ashton-honnecke-role'
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Get task IP")
-    parser.add_argument("service", help="Service name, or partial service name")
-    parser.add_argument("-c", "--cluster", help="The cluster to query")
-    parser.add_argument("-t", "--task", help="The task to query")
+    parser = argparse.ArgumentParser(description='Get task IP')
+
+    parser.add_argument(
+        'service',
+        help='Service name, or partial service name'
+    )
+
+    parser.add_argument(
+        '-c',
+        '--cluster',
+        help='The cluster to query'
+    )
+
+    parser.add_argument(
+        '-t',
+        '--task',
+        help='The task to query'
+    )
 
     return parser.parse_args()
 
@@ -31,17 +45,18 @@ def auth():
 
     # create an STS client object that represents a live connection to the
     # STS service
-    sts_client = boto3.client("sts")
+    sts_client = boto3.client('sts')
 
     # Call the assume_role method of the STSConnection object and pass the role
     # ARN and a role session name.
     assumedRoleObject = sts_client.assume_role(
-        RoleArn=WEBARN, RoleSessionName="AssumeRoleSession1"
+        RoleArn=WEBARN,
+        RoleSessionName="AssumeRoleSession1"
     )
 
     # From the response that contains the assumed role, get the temporary
     # credentials that can be used to make subsequent API calls
-    credentials = assumedRoleObject["Credentials"]
+    credentials = assumedRoleObject['Credentials']
 
     # # Use the temporary credentials that AssumeRole returns to make a
     # # connection to Amazon S3
@@ -58,9 +73,9 @@ def auth():
     #     print(bucket.name)
 
     return {
-        "aws_access_key_id": credentials["AccessKeyId"],
-        "aws_secret_access_key": credentials["SecretAccessKey"],
-        "aws_session_token": credentials["SessionToken"],
+        'aws_access_key_id': credentials['AccessKeyId'],
+        'aws_secret_access_key': credentials['SecretAccessKey'],
+        'aws_session_token': credentials['SessionToken'],
     }
 
 
@@ -68,11 +83,11 @@ if __name__ == "__main__":
     args = get_args()
     creds = auth()
 
-    ecs = boto3.client("ecs", **creds)
+    ecs = boto3.client('ecs', **creds)
 
     serviceArns = {}
-    for arn in ecs.list_clusters().get("clusterArns"):
-        serviceArns[arn] = ecs.list_services(cluster=arn).get("serviceArns")
+    for arn in ecs.list_clusters().get('clusterArns'):
+        serviceArns[arn] = ecs.list_services(cluster=arn).get('serviceArns')
 
     for cluster, services in serviceArns.items():
         for service in services:
@@ -81,17 +96,16 @@ if __name__ == "__main__":
                 service_match = service
                 cluster_match = cluster
 
-    tasks = ecs.list_tasks(cluster=cluster_match).get("taskArns")
+    tasks = ecs.list_tasks(cluster=cluster_match).get('taskArns')
 
-    response = ecs.describe_tasks(cluster=cluster_match, tasks=tasks)
+    response = ecs.describe_tasks(
+        cluster=cluster_match,
+        tasks=tasks
+    )
 
     # print(f'{cluster_match}:::{service_match}')
-    for task_detail in response.get("tasks"):
-        if args.service in task_detail.get("group"):
-            internal_ip = (
-                task_detail.get("containers")[0]
-                .get("networkInterfaces")[0]
-                .get("privateIpv4Address")
-            )
+    for task_detail in response.get('tasks'):
+        if args.service in task_detail.get('group'):
+            internal_ip = task_detail.get('containers')[0].get('networkInterfaces')[0].get('privateIpv4Address')
 
     print(internal_ip)
