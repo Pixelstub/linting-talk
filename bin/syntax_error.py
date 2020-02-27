6021fcc128fcbf96544367a520cfbff3b510db23
Ashton Honnecke <ashton@digitalassetsdata.com>
2020-02-27 16:00:15 -0700

    add files with errors

---
+++ bin/syntax_error.py
@@ -0,0 +1,10 @@
+from web3 import Web3
+import web3
+import logging
+import os
+import requests
+
+infura_token = "59d7fd40b0b94a9a9ce4dcab99c1f069"
+w3 = Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{token}"))
+
+print(w3.eth.blockNumber)
